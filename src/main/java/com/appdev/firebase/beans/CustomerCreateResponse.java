a47fbc4356d70895d72c278597e76ca71306914b
Saloni Agrawal <salonia9505@gmail.com>
2025-01-28 00:55:53 +0530

    added name variable

--- src/main/java/com/appdev/firebase/beans/CustomerCreateResponse.java
+++ src/main/java/com/appdev/firebase/beans/CustomerCreateResponse.java
@@ -6,21 +6,25 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CustomerCreateResponse {
-	
+
 	private String eid;
+	private String name;
 	private Date updatedTime;
+
 	public String getEid() {
 		return eid;
 	}
+
 	public void setEid(String eid) {
 		this.eid = eid;
 	}
+
 	public Date getUpdatedTime() {
 		return updatedTime;
 	}
+
 	public void setUpdatedTime(Date updatedTime) {
 		this.updatedTime = updatedTime;
 	}
-	
-	
+
 }
